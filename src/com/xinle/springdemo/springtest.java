62635a2b770bb3fe377995a56b708beb78c4d996
mypingqiu <18338456560@163.com>
2022-03-15 15:11:42 +0800

    v 1.0.0 spring项目 #简单Java代码，测试正常执行

---
+++ src/com/xinle/springdemo/springtest.java
@@ -0,0 +1,34 @@
+package com.xinle.springdemo;
+
+/**
+ * Copyright (c) 2008-2028 考呀呀 All Rights Reserved
+ *
+ * @Project: spring_project
+ * @Package: com.xinle.springdemo
+ * @Mail: 18338456560@163.com
+ * @Version: V1.0
+ * @TODO: Created by cxl on 2022/3/15 3:05 PM
+ */
+
+/**
+ * 测试spring代码
+ */
+public class springtest {
+
+    public void  addtest(){
+        System.out.println("add....");
+    }
+
+
+    /**
+     * 测试调用addtest方法
+     * @param args
+     */
+    public static void main(String[] args) {
+        springtest st = new springtest();
+        st.addtest();
+    }
+
+
+
+}
